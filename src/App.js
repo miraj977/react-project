051395069b0f39d0a9ef7890a3bfe63cabbcdcde
Miraj <mirajaryal@gmail.com>
2021-02-07 18:30:31 +1100

    Hero Section Styled. Creating Card Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App ()
 {
   return (
-    <div className="App">
+    <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
-    </div>
+    </>
   );
 }
 
